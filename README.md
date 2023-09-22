68004b10c09381c428389ed24d52dca3021a7284
JoJo0217 <mingu0217@gmail.com>
2023-09-22 20:57:44 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+polyglot model의 8bit 양자화 training code입니다.   
+tloen/alpaca-lora 코드를 바탕으로 kullm에서 수정한 코드의 peft weight 저장 오류, loss 반복 등을 고친 코드입니다.   
+모델 학습 후 testkullm.py로 gradio test 까지 가능하게 만들어 두었습니다.
---
+++ train_polyglot.sh
@@ -0,0 +1,22 @@
+python finetune_polyglot.py \
+    --base_model 'EleutherAI/polyglot-ko-5.8b' \
+    --data_path data/step1_inst_format.jsonl \
+    --output_dir ckpt/ \
+    --prompt_template_name kullm \
+    --batch_size 128 \
+    --micro_batch_size 4 \
+    --num_epochs 3 \
+    --learning_rate 5e-4 \
+    --cutoff_len 1024 \
+    --val_set_size 0 \
+    --lora_r 8 \
+    --lora_alpha 16 \
+    --lora_dropout 0.05 \
+    --lora_target_modules "[query_key_value, xxx]" \
+    --train_on_inputs \
+    --eval_steps 0 \
+    --weight_decay 0.1 \
+    --warmup_steps 0 \
+    --warmup_ratio 0.1 \
+    --lr_scheduler_type "cosine"
+
